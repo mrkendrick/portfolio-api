6e35ac6702b1e2ed3653bb9fb6c7e446c6713f83
Charles Duruaku <charlesrichson@gmail.com>
2022-08-31 13:56:43 +0100

    added skill and socils entities

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,27 @@
+// export default ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "strapi"),
+//       user: env("DATABASE_USERNAME", "strapi"),
+//       password: env("DATABASE_PASSWORD", "strapi"),
+//     },
+//     useNullAsDefault: true,
+//   },
+//   debug: true,
+// });
+
 export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      host: env("LOCAL_DATABASE_HOST", "127.0.0.1"),
+      port: env.int("LOCAL_DATABASE_PORT", 5432),
+      database: env("LOCAL_DATABASE_NAME", "strapi"),
+      user: env("LOCAL_DATABASE_USERNAME", "strapi"),
+      password: env("LOCAL_DATABASE_PASSWORD", "strapi"),
     },
     useNullAsDefault: true,
   },

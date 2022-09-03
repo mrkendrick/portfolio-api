9dfa3371f01be787c8b370970b124e33aec46164
Charles Duruaku <charlesrichson@gmail.com>
2022-09-03 02:28:18 +0100

    removed pro lang entity

--- dist/src/api/programming-language/controllers/programming-language.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * programming-language controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::programming-language.programming-language');

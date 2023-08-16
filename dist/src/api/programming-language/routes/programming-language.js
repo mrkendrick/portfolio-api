df625370a080cf9d67aea842413abdd5617a97d6
Charles Duruaku <charlesrichson@gmail.com>
2023-08-16 09:56:19 +0100

    bumbed up node version

--- dist/src/api/programming-language/routes/programming-language.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * programming-language router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::programming-language.programming-language');
